1583129b74cf630829dc8eba61c3a403539ea62e
Jeffrey Sewell <77352230+jeffreysewell@users.noreply.github.com>
2023-03-18 02:02:55 -0400

    Push... and then pull

--- App.js
+++
@@ -1,9 +0,0 @@
-import { WordRotator } from './WordRotator.js';
-
-const rotatingWordElement = document.getElementById('rotating-word');
-const words = ['Welcome', 'Willkommen', '欢迎', 'Benvenuto', 'いらっしゃいませ', 'Bem-vindo', 'Добро пожаловать', 'Bienvenido'];
-// Replace with your desired words
-const duration = 2000;
-// Replace with your desired duration in milliseconds
-
-new WordRotator(rotatingWordElement, words, duration);
