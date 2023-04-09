32e22943d1b2c1ead75003ff53ff06b322fb55f6
Jeffrey Sewell <77352230+jeffreysewell@users.noreply.github.com>
2023-04-09 02:13:19 -0400

    js mod

--- js/script.js
+++ js/script.js
@@ -6,8 +6,20 @@ function setTextColorBasedOnBackground(backgroundColor) {
   // ... (the rest of the setTextColorBasedOnBackground function)
 }
 
-function typeText(element, text, interval) {
-  // ... (the rest of the typeText function)
+function typeText(element, text, interval, onComplete) {
+  let index = 0;
+
+  const typing = () => {
+    if (index < text.length) {
+      element.textContent += text.charAt(index);
+      index++;
+      setTimeout(typing, interval);
+    } else if (onComplete) {
+      onComplete();
+    }
+  };
+
+  typing();
 }
 
 // Get the current background image of the body
@@ -23,17 +35,20 @@ fetch(backgroundImage.slice(5, -2))
 
     img.onload = () => {
       // ... (the rest of the img.onload function)
-
-      // Get the h1 element
-      const typedTextElement = document.getElementById("typed-text");
-
-      // Set the text you want to appear in the h1 element
-      const textToType = "Willkommen im Asheville Haus";
-
-      // Set the typing speed (interval in milliseconds between each character)
-      const typingSpeed = 100;
-
-      // Type the text
-      typeText(typedTextElement, textToType, typingSpeed);
     };
+
+    // Get the h1 element
+    const typedTextElement = document.getElementById("typed-text");
+
+    // Set the text you want to appear in the h1 element
+    const textToType = "Willkommen im Asheville Haus";
+
+    // Set the typing speed (interval in milliseconds between each character)
+    const typingSpeed = 100;
+
+    // Type the text
+    typeText(typedTextElement, textToType, typingSpeed, () => {
+      // Remove aria-hidden attribute after typing is completed
+      typedTextElement.removeAttribute("aria-hidden");
+    });
   });
