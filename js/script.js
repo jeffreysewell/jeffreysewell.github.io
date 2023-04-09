14676163d7c0e3a9569777d0b367ba1abcbdca62
Jeffrey Sewell <77352230+jeffreysewell@users.noreply.github.com>
2023-04-09 01:35:57 -0400

    Add typed text element

--- js/script.js
+++ js/script.js
@@ -1,46 +1,39 @@
 function getLuminance(r, g, b) {
-    const a = [r, g, b].map((v) => {
-      v /= 255;
-      return v <= 0.03928 ? v / 12.92 : ((v + 0.055) / 1.055) ** 2.4;
-    });
-    return 0.2126 * a[0] + 0.7152 * a[1] + 0.0722 * a[2];
-  }
-  
-  function setTextColorBasedOnBackground(backgroundColor) {
-    const rgb = backgroundColor.match(/\d+/g).map(Number);
-    const luminance = getLuminance(rgb[0], rgb[1], rgb[2]);
-    const textColor = luminance > 0.5 ? "black" : "white";
-  
-    const body = document.getElementById("body");
-    body.style.color = textColor;
-  }
-  
-  // Get the current background image of the body
-  const body = document.getElementById("body");
-  const backgroundImage = getComputedStyle(body).getPropertyValue("background-image");
-  
-  // Fetch the image and get the average color
-  fetch(backgroundImage.slice(5, -2))
-    .then((response) => response.blob())
-    .then((blob) => {
-      const img = new Image();
-      img.src = URL.createObjectURL(blob);
-  
-      img.onload = () => {
-        const canvas = document.createElement("canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0, img.width, img.height);
-  
-        // Get the average color of the background image
-        const data = ctx.getImageData(0, 0, img.width, img.height).data;
-        const r = data[0];
-        const g = data[1];
-        const b = data[2];
-  
-        const backgroundColor = `rgb(${r}, ${g}, ${b})`;
-        setTextColorBasedOnBackground(backgroundColor);
-      };
-    });
-  
+  // ... (the rest of the getLuminance function)
+}
+
+function setTextColorBasedOnBackground(backgroundColor) {
+  // ... (the rest of the setTextColorBasedOnBackground function)
+}
+
+function typeText(element, text, interval) {
+  // ... (the rest of the typeText function)
+}
+
+// Get the current background image of the body
+const body = document.getElementById("body");
+const backgroundImage = getComputedStyle(body).getPropertyValue("background-image");
+
+// Fetch the image and get the average color
+fetch(backgroundImage.slice(5, -2))
+  .then((response) => response.blob())
+  .then((blob) => {
+    const img = new Image();
+    img.src = URL.createObjectURL(blob);
+
+    img.onload = () => {
+      // ... (the rest of the img.onload function)
+    };
+  });
+
+// Get the h1 element
+const typedTextElement = document.getElementById("typed-text");
+
+// Set the text you want to appear in the h1 element
+const textToType = "Willkommen im Asheville Haus";
+
+// Set the typing speed (interval in milliseconds between each character)
+const typingSpeed = 100;
+
+// Type the text
+typeText(typedTextElement, textToType, typingSpeed);
